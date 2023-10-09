9d86dc3fac2133c8c894a7a2f18f2b75848503c8
Zhihao Fu <120171892+EdmundFu-233@users.noreply.github.com>
2023-10-09 17:48:00 +0000

    add helper 20

---
+++ utils/helper_20.py
@@ -0,0 +1,19 @@
+"""Utility module 20"""
+import os
+import sys
+from typing import Optional
+
+def get_env(key: str, default: Optional[str] = None) -> Optional[str]:
+    return os.environ.get(key, default)
+
+def get_python_version() -> str:
+    return sys.version
+
+def is_python_3() -> bool:
+    return sys.version_info.major >= 3
+
+def greet(name: str) -> str:
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    print(get_python_version())
